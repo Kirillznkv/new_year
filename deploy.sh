7f1c8c6aaab356b4cd32f7b30ea2c01a03564fc4
kirillznkv <Zenich99@gmail.com>
2022-12-27 12:06:51 +0600

    fix for hosting

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/pressly/goose/master/install.sh
-curl apt install docker.io
-curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -fsSL https://raw.githubusercontent.com/pressly/goose/master/install.sh;
+curl -fsSL https://get.docker.com;
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose;
 
 make stop;
 
