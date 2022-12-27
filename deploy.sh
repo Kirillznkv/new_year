3098fdcfbf3fc9646e56f25491609c47cecc371f
kirillznkv <Zenich99@gmail.com>
2022-12-27 11:36:48 +0600

    fix for hosting

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+curl -fsSL https://raw.githubusercontent.com/pressly/goose/master/install.sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg
+curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
 make stop;
 
 mkdir images;
